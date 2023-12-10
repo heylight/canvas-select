1a2b565304217edccc1e55889c243c80bf4a3f80
liuz <liuz@qq.com>
2023-12-10 22:43:56 +0800

    feat: 优化

--- src/tools.ts
+++ src/tools.ts
@@ -1,21 +1,20 @@
 export function createUuid(): string {
-    const s: any[] = [];
-    const hexDigits = '0123456789abcdef';
-    for (let i = 0; i < 36; i++) {
-        const m = Math.floor(Math.random() * 0x10)
-        s[i] = hexDigits.slice(m, m + 1);
-    }
-    s[14] = '4';
-    const n = (s[19] & 0x3) | 0x8
-    s[19] = hexDigits.slice(n, n + 1);
-    s[8] = s[13] = s[18] = s[23] = '-';
-    const uuid = s.join('');
-    return uuid;
+  const s: any[] = [];
+  const hexDigits = "0123456789abcdef";
+  for (let i = 0; i < 36; i++) {
+    const m = Math.floor(Math.random() * 0x10);
+    s[i] = hexDigits.slice(m, m + 1);
+  }
+  s[14] = "4";
+  const n = (s[19] & 0x3) | 0x8;
+  s[19] = hexDigits.slice(n, n + 1);
+  s[8] = s[13] = s[18] = s[23] = "-";
+  const uuid = s.join("");
+  return uuid;
 }
 
 /**
- * 判断图形是否符合嵌套关系, 业务需求：只需要判断shape2是否在shape1内部即可
- * (目前只支持 矩形 多边形的判断)
+ * 判断图形是否符合嵌套关系, 业务需求：只需要判断shape2所有的点是否都在shape1内部即可
  * @param shape1 参数1
  * @param shape2 参数2
  * @reutrn Boolean 符合条件返回true 否则返回false
@@ -36,7 +35,7 @@ export function isNested(shape1: any, shape2: any): boolean {
       return false; // 两个矩形没有嵌套关系
     }
   } else if (shape1.type === 1 && shape2.type === 2) {
-    // 矩形和多边形的判断逻辑
+    // 矩形和多边形的判断逻辑，确保多边形所有的坐标点都在矩形里面
     const [[x1, y1], [x2, y2]] = shape1.coor;
     const vertices = shape2.coor;
 
@@ -49,29 +48,21 @@ export function isNested(shape1: any, shape2: any): boolean {
 
     return true; // 所有顶点都在矩形内部，嵌套关系成立
   } else if (shape1.type === 2 && shape2.type === 1) {
-    // 多边形和矩形的判断逻辑
-    const [[x1, y1], [x2, y2]] = shape2.coor;
-    const vertices = shape1.coor;
+    // 多边形和矩形的判断逻辑，确保矩形的所有坐标点都在多边形里面
+    const vertices = shape2.coor; // 矩形的顶点坐标
 
     for (let i = 0; i < vertices.length; i++) {
       const [x, y] = vertices[i];
-      if (x < x1 || x > x2 || y < y1 || y > y2) {
-        return true; // 多边形的顶点在矩形外部，嵌套关系成立
+      if (!isPointInPolygon(x, y, shape1.coor)) {
+        return false; // 有一个坐标点不在多边形范围内，返回false
       }
     }
 
-    return false; // 所有顶点都在矩形内部，不嵌套
+    return true; // 所有坐标点都在多边形内部，返回true
   } else if (shape1.type === 2 && shape2.type === 2) {
     // 多边形和多边形的判断逻辑
     const vertices1 = shape1.coor;
     const vertices2 = shape2.coor;
-
-    // for (let i = 0; i < vertices1.length; i++) {
-    //   const [x, y] = vertices1[i];
-    //   if (!isPointInPolygon(x, y, vertices2)) {
-    //     return false; // 多边形1的顶点不都在多边形2内部，不嵌套
-    //   }
-    // }
 
     for (let i = 0; i < vertices2.length; i++) {
       const [x, y] = vertices2[i];
@@ -80,7 +71,7 @@ export function isNested(shape1: any, shape2: any): boolean {
       }
     }
 
-    return true; // 两个多边形的顶点都在对方内部，嵌套关系成立
+    return true; // 有坐标点都在多边形内部，返回true
   }
 }
 
@@ -89,8 +80,10 @@ function isPointInPolygon(x: number, y: number, vertices: any) {
   const n = vertices.length;
 
   for (let i = 0, j = n - 1; i < n; j = i++) {
-    const [xi, yi] = vertices[i];
-    const [xj, yj] = vertices[j];
+    const xi = vertices[i][0];
+    const yi = vertices[i][1];
+    const xj = vertices[j][0];
+    const yj = vertices[j][1];
 
     if (yi > y !== yj > y && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi) {
       inside = !inside;
