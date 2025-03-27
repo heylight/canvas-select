d19cb1a949354d5677d7ee387ee241b679774794
Zhu Hailiang <zaib-zhuhailiang@zhonganib.com>
2025-03-27 10:04:45 +0800

    feat: 更新插件

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from 'rollup-plugin-typescript2';
 import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import json from '@rollup/plugin-json';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 
 const mergePlugins = process.env.NODE_ENV === 'development' ?
   [serve({ open: true }), livereload({ watch: 'lib' })] : [terser()]
