31c0c289dc08f7cfcb314b56863e43f0bbc779f0
Zhu Hailiang <zaib-zhuhailiang@zhonganib.com>
2025-05-09 14:35:05 +0800

    doc: 更新文档

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,60 @@
-import babel from '@rollup/plugin-babel';
-import typescript from 'rollup-plugin-typescript2';
-import serve from 'rollup-plugin-serve';
-import livereload from 'rollup-plugin-livereload';
-import json from '@rollup/plugin-json';
-import terser from '@rollup/plugin-terser';
+import babel from "@rollup/plugin-babel";
+import typescript from "rollup-plugin-typescript2";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import json from "@rollup/plugin-json";
+import terser from "@rollup/plugin-terser";
 
-const mergePlugins = process.env.NODE_ENV === 'development' ?
-  [serve({ open: true }), livereload({ watch: 'lib' })] : [terser()]
+const mergePlugins =
+  process.env.NODE_ENV === "development"
+    ? [serve({ open: true }), livereload({ watch: "lib" })]
+    : [terser()];
+
+const pkg = JSON.parse(
+  await import("fs").then((fs) => fs.promises.readFile("package.json", "utf8"))
+);
+
+const banner = `/*!
+ * ${pkg.name} v${pkg.version}
+ * ${pkg.description}
+ * (c) ${new Date().getFullYear()} ${pkg.author}
+ * Released under the ${pkg.license} License.
+ */`;
 
 const baseConfig = {
-  input: 'src/index.ts',
+  input: "src/index.ts",
   plugins: [
-    babel({ babelHelpers: 'bundled' }),
+    babel({ babelHelpers: "bundled" }),
     typescript({
-      tsconfig: "tsconfig.json"
+      tsconfig: "tsconfig.json",
     }),
     json(),
-    ...mergePlugins
-  ]
-}
+    ...mergePlugins,
+  ],
+};
 
 export default [
   // UMD 构建
   {
     ...baseConfig,
     output: {
-      file: 'lib/canvas-select.min.js',
-      format: 'umd',
-      name: 'CanvasSelect',
-      exports: 'auto',
+      file: "lib/canvas-select.min.js",
+      format: "umd",
+      name: "CanvasSelect",
+      exports: "auto",
       sourcemap: true,
-    }
+      banner,
+    },
   },
   // ES 构建
   {
     ...baseConfig,
     output: {
-      file: 'lib/canvas-select.esm.js',
-      format: 'es',
-      exports: 'auto',
-      sourcemap: true
-    }
-  }
+      file: "lib/canvas-select.esm.js",
+      format: "es",
+      exports: "auto",
+      sourcemap: true,
+      banner,
+    },
+  },
 ];
